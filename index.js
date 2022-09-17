23f9d1b57ed39a5e42786841197aa065e8d9af87
Óscar Rabadán Martínez <rabadanoscar@hotmail.com>
2022-09-17 15:19:31 +0200

    Added comments, improvings in code
    Se ha comentado el HTML, CSS y Javascript así como optimizado la estructura del código CSS y dividido en distintos archivos. Código JS actualizado con funciones flecha.

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-const slider = document.querySelector("#slider");
+// Guardamos en variables los elementos necesarios
+const slider = document.getElementById("slider");
 let sliderSection = document.querySelectorAll(".slider-section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
 
-const btnLeft = document.querySelector("#btn-left");
-const btnRight = document.querySelector("#btn-right");
+// Función para pasar a la siguiente imagen
+const Next = () => {
+  let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+};
 
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+// Función para pasar a la imagen anterior
+const Prev = () => {
+  let sliderSection = document.querySelectorAll(".slider-section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+};
 
-function Next() {
-    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginLeft = "-100%";
-    }, 500);
-}
-
-function Prev() {
-    let sliderSection = document.querySelectorAll(".slider-section");
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
-    slider.style.marginLeft = "0";
-    slider.style.transition ="all 0.5s";
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-        slider.style.marginLeft = "-100%";
-    }, 500);
-}
-
-btnRight.addEventListener('click', function() {
-    Next();
+// Damos funcionalidad a los botones
+btnRight.addEventListener("click", () => {
+  Next();
 });
 
-btnLeft.addEventListener('click', function() {
-    Prev();
+btnLeft.addEventListener("click", () => {
+  Prev();
 });
 
-setInterval(function(){
-    Next();
+// Función para pasar a la siguiente imágen de forma automática cada cinco segundos
+setInterval(() => {
+  Next();
 }, 5000);
